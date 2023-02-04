ea6cfd822d661d0192f3627b165c6881690f842d
tkgs0 <hgc.sakuya@ya.ru>
2023-02-04 17:42:16 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,112 @@
+<div align="center">
+  <a href="https://v2.nonebot.dev/store"><img src="https://raw.githubusercontent.com/tkgs0/nbpt/resources/nbp_logo.png" width="180" height="180" alt="NoneBotPluginLogo"></a>
+  <br>
+  <p><img src="https://raw.githubusercontent.com/tkgs0/nbpt/resources/NoneBotPlugin.svg" width="240" alt="NoneBotPluginText"></p>
+</div>
+
+<div align="center">
+
 # nonebot-plugin-dicky-pk
-群聊小游戏: CHINCHIN-PK
+
+_✨ NoneBot Dicky-PK ✨_
+
+
+<a href="./LICENSE">
+    <img src="https://img.shields.io/github/license/tkgs0/nonebot-plugin-dicky-pk.svg" alt="license">
+</a>
+<a href="https://pypi.python.org/pypi/nonebot-plugin-dicky-pk">
+    <img src="https://img.shields.io/pypi/v/nonebot-plugin-dicky-pk.svg" alt="pypi">
+</a>
+<a href="https://www.python.org">
+    <img src="https://img.shields.io/badge/python-3.8+-blue.svg" alt="python">
+</a>
+
+</div>
+
+## 📖 介绍
+
+魔改自 OPQBot 的群聊小游戏 [CHINCHIN-PK](https://github.com/opq-osc/chinchin-pk)
+
+## 💿 安装
+
+**nb-cli安装, 包管理器安装  二选一**
+
+<details>
+<summary>使用 nb-cli 安装</summary>
+
+在 nonebot2 项目的根目录下打开命令行, 输入以下指令即可安装
+
+    nb plugin install nonebot-plugin-dicky-pk
+
+</details>
+
+<details>
+<summary>使用包管理器安装</summary>
+
+在 nonebot2 项目的插件目录下, 打开命令行,
+
+**根据你使用的包管理器, 输入相应的安装命令**
+
+<details>
+<summary>pip</summary>
+
+    pip install nonebot-plugin-dicky-pk
+
+</details>
+<details>
+<summary>pdm</summary>
+
+    pdm add nonebot-plugin-dicky-pk
+
+</details>
+<details>
+<summary>poetry</summary>
+
+    poetry add nonebot-plugin-dicky-pk
+
+</details>
+<details>
+<summary>conda</summary>
+
+    conda install nonebot-plugin-dicky-pk
+
+</details>
+
+打开 bot项目下的 `pyproject.toml` 文件,
+
+在其 `plugins` 里加入 `nonebot_plugin_dicky_pk`
+
+    plugins = ["nonebot_plugin_dicky_pk"]
+
+</details>
+</details>
+
+## ⚙️ 配置
+
+首次运行该插件会生成 `Dicky_PK.json` 到Bot项目的 `configs` 文件夹里,
+
+可根据 `Dicky_PK.json` 内的注释进行配置.
+
+## 🎉 使用
+
+### 指令表
+
+**注意:**
+
+以下命令不可省略前缀 `/`
+
+如果bot项目的 `.env` 里只设置了前缀 `/`, 那么你发送以下命令时前缀应该是 `//`
+
+```
+/开启(关闭)牛子秘境         # 插件总开关
+/启用(禁用)牛子pk           # 插件在群聊的开关
+/牛子                       # 查看自己的牛子信息
+/pk @用户                   # 对at的用户进行牛子PK, 一次只能PK一个用户
+/🔒(/suo/嗦/锁)我           # 嗦自己的牛子
+/🔒(/suo/嗦/锁) @用户       # 嗦at用户的牛子
+/打胶                       # 安抚自己的牛子
+/看他牛子(/看看牛子) @用户  # 查看at用户的牛子
+/注册牛子                   # 注册属于你的牛子
+/牛子排名(/牛子排行)        # 查看牛子排行榜
+```
+
---
+++ nonebot_plugin_dicky_pk/__init__.py
@@ -0,0 +1,160 @@
+from pathlib import Path
+try:
+    import ujson as json
+except ModuleNotFoundError:
+    import json
+from nonebot import on_message, on_command
+from nonebot.matcher import Matcher
+from nonebot.permission import SUPERUSER
+from nonebot.adapters.onebot.v11 import (
+    Bot,
+    MessageEvent,
+    GroupMessageEvent,
+    GROUP_OWNER,
+    GROUP_ADMIN,
+)
+
+from .src.main import KEYWORDS, message_processor as chinchin
+
+
+confpath = Path() / 'data' / 'chinchin_pk' / 'chinchin.json'
+confpath.parent.mkdir(parents=True, exist_ok=True)
+
+enablelist = (
+    json.loads(confpath.read_text(encoding='utf-8'))
+    if confpath.is_file()
+    else {'all': False, 'group': []}
+)
+
+
+def save_conf():
+    confpath.write_text(json.dumps(enablelist), encoding='utf-8')
+
+
+_help = [
+    '/牛子',
+    '/pk @用户',
+    '/🔒(/suo/嗦/锁)我',
+    '/🔒(/suo/嗦/锁) @用户',
+    '/打胶',
+    '/看他牛子(/看看牛子) @用户',
+    '/注册牛子',
+    '/牛子排名(/牛子排行)',
+]
+
+
+jjpk = on_message(priority=96, block=False)
+
+@jjpk.handle()
+async def _(bot: Bot, event: GroupMessageEvent, matcher: Matcher):
+    if not enablelist['all']:
+        return
+    if not event.group_id in enablelist['group']:
+        return
+    msg = event.get_plaintext()
+    if msg.startswith('/'):
+        msg = msg.replace('/', '', 1)
+        x = 0
+        for i in KEYWORDS.values():
+            for j in i:
+                if j in msg:
+                    x = 1
+        if not x:
+            return
+        uid = event.user_id
+        gid = event.group_id
+        uids = [at.data['qq'] for at in event.get_message()['at']]
+        at_id = uids[0] if uids else None
+        nickname = event.sender.card if event.sender.card else event.sender.nickname
+        fuzzy_match = True
+        chinchin(bot, matcher, msg, uid, gid, at_id, nickname, fuzzy_match)
+
+
+jjpk_help = on_command(
+    '/牛子帮助',
+    priority=5,
+    block=True
+)
+
+@jjpk_help.handle()
+async def _(event: GroupMessageEvent):
+    if not enablelist['all']:
+        return
+    if not event.group_id in enablelist['group']:
+        return
+    await jjpk_help.finish('\n'.join(_help))
+
+
+def set_enable(gid: int, en: bool):
+    if en:
+        enablelist['group'].append(gid)
+        list(set(enablelist['group']))
+    else:
+        enablelist['group'] = [uid for uid in enablelist['group'] if not uid == gid]
+    save_conf()
+
+
+
+enable_jjpk = on_command(
+    '/启用牛子pk',
+    aliases={'/开启牛子pk', '/启用dicky-pk', '/开启dicky-pk'},
+    permission=GROUP_ADMIN | GROUP_OWNER | SUPERUSER,
+    priority=5,
+    block=True
+)
+
+@enable_jjpk.handle()
+async def _(event: GroupMessageEvent):
+    if not enablelist['all']:
+        return
+    set_enable(event.group_id, True)
+    await enable_jjpk.finish('已启用群聊小游戏: Dicky-PK')
+
+
+disable_jjpk = on_command(
+    '/禁用牛子pk',
+    aliases={'/关闭牛子pk', '/禁用dicky-pk', '/关闭dicky-pk'},
+    permission=GROUP_ADMIN | GROUP_OWNER | SUPERUSER,
+    priority=5,
+    block=True
+)
+
+@disable_jjpk.handle()
+async def _(event: GroupMessageEvent):
+    if not enablelist['all']:
+        return
+    set_enable(event.group_id, False)
+    await disable_jjpk.finish('已禁用群聊小游戏: Dicky-PK')
+
+
+chinchin_enable = on_command(
+    '/开启牛子秘境',
+    permission=SUPERUSER,
+    priority=2,
+    block=True
+)
+
+@chinchin_enable.handle()
+async def _(event: MessageEvent):
+    msg = ''
+    if isinstance(event, GroupMessageEvent):
+        set_enable(event.group_id, True)
+        msg += '\n已在本群启用牛子pk'
+    enablelist['all']  = True
+    save_conf()
+    await chinchin_enable.finish('牛子秘境已开启.'+msg)
+
+
+chinchin_disable = on_command(
+    '/关闭牛子秘境',
+    permission=SUPERUSER,
+    priority=2,
+    block=True
+)
+
+@chinchin_disable.handle()
+async def _():
+    enablelist['group'].clear()
+    enablelist['all']  = False
+    save_conf()
+    await chinchin_disable.finish('牛子秘境已关闭.')
